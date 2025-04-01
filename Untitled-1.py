e8061743b85b0f2c7e75b6673243b75442814ec5
Inqvis <gordololero202@gmail.com>
2025-04-01 17:35:18 -0300

    calc

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,1 +1,31 @@
-print("hello")
+def Multiplicacion():
+    resultado = chokko * wafle
+    print(resultado)
+def Division():
+    resultado = chokko / wafle
+    print(resultado)
+def Suma():
+    resultado = chokko + wafle
+    print(resultado)
+def Resta():
+    resultado = chokko - wafle
+    print(resultado)
+
+
+     
+print("Calculadora")
+chokko = float(input("Ingrese el primer valor: "))
+Monoko = input("Ingrese el operador")
+wafle = float(input("Ingrese el segundo valor: "))
+if Monoko == "*":
+    Multiplicacion()
+if Monoko == "/":
+    if wafle == 0:
+        print("No se divide entre 0")
+        exit
+    else:
+       Division() 
+if Monoko == "+":
+    Suma()
+if Monoko == "-":
+    Resta()
